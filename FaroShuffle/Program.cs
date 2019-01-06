e0708704193aa4d9ef3a589ce4e6712f31031902
Nick Doran <chasedoran6@gmail.com>
2019-01-06 13:17:10 -0500

    Add project files.

---
+++ FaroShuffle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FaroShuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
